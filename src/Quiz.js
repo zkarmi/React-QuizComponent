65b23e207a91adf5fbba51ec1c131f221713a2dd
Ziv Karmi <ziv.karmi@bluecore.com>
2022-01-04 21:26:16 -0500

    module 1

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 
 let quizData = require('./quiz_data.json');
-
-export default class Quiz extends Component {
+class Quiz extends Component {
 
   constructor(props) {
     super(props);
@@ -19,3 +18,5 @@ export default class Quiz extends Component {
     )
   }
 }
+
+export default Quiz;
