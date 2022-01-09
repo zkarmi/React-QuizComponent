63bf0a7a745f3472708c0f67ab27501b17258533
Ziv Karmi <ziv.karmi@bluecore.com>
2022-01-08 23:10:06 -0500

    module 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -30,7 +30,7 @@ class QuizQuestion extends Component {
             ) }
           </ul>
         </section>
-        { this.state.incorrectAnswer ? <p>Sorry, that's not right</p> : null }
+        { this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null }
       </main>
     )
   }
